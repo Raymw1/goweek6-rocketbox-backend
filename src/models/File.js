3c9edd02ec900b460b0b96c740d028fc3190298c
Raymw1 <rayan.rmw@gmail.com>
2021-10-05 18:41:24 -0300

    Models of application done

---
+++ src/models/File.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const FileSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    path: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("File", FileSchema);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
